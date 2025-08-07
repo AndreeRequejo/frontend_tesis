403410318ec2b43dd8181fcfd71bc1ff962e79e0
Andree Requejo <andree09requejo@gmail.com>
2025-08-06 22:25:24 -0500

    modify actualizar estilos y estructura del botón en el dashboard

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
@@ -14,15 +15,15 @@ export default function Dashboard() {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Dashboard" />
-            <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
-                {/* <div className="from-medical-blue to-medical-indigo rounded-lg bg-gradient-to-r p-5 text-white shadow-lg">
+            <div className='m-4 flex flex-col'>
+                <div className="rounded-lg bg-gradient-to-r from-blue-500 to-indigo-600 p-5 text-white shadow-lg">
                     <h2 className="mb-1 text-xl font-semibold">Bienvenido</h2>
                     <p className="mb-4 text-blue-100">Sistema de clasificación de gravedad de acné mediante IA</p>
-                    <button className="text-medical-blue w-full bg-white hover:bg-blue-50">
+                    <Button variant="secondary" className="w-full text-primary">
                         <Camera size={18} className="mr-2" />
                         Nueva Evaluación
-                    </button>
-                </div> */}
+                    </Button>
+                </div>
             </div>
         </AppLayout>
     );
