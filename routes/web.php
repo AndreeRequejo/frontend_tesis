53354e85afc678bde3860cc022d02b320c3001d0
Andree Requejo <andree09requejo@gmail.com>
2025-08-09 16:30:38 -0500

    feat: ajustar rutas de autenticación para mejorar la navegación y el acceso a la página de inicio de sesión

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('auth/login');
-})->name('login');
-
 Route::middleware(['auth'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
